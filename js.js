38470367fd126fa1d745057e2b5f78911f7069a2
Anton Midtsundstad <anton.midtsundstad@gmail.com>
2024-04-24 13:44:31 +0200

    ujn

--- js.js
+++ js.js
@@ -2,4 +2,22 @@
 
 function queSystem() {
     
+    let button = document.querySelector("#ställ_i_kö");
+    let name = document.querySelector("#namn").value;
+    let quantity = document.querySelector("#antal").value;
+    let outputbox = document.querySelector("#outputbox");
+
+    button.addEventListener("click", function(){
+        let p = document.createElement("p");
+        document.outputbox.appendChild(p);
+        p.textContent = name + quantity
+
+        p.addEventListener("contextmenu",function giveTable(){
+            if(anyTable == free){
+                alert(this.textContent);
+                this.removeChild;
+                this.anyTable.style.backgroundcolor = "red";
+            }
+        })
+    })
 }
